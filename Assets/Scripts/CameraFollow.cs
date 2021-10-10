2bff42f9b7360d2a5237ef092090f0d3332c38e2
Mark Wiemer <markwiemer@outlook.com>
2021-10-10 11:07:39 -0700

    Add camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 tempPos;
+
+    [SerializeField]
+    private float minX, maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        tempPos = transform.position;
+        float playerX = player.position.x;
+        tempPos.x = Mathf.Min(Mathf.Max(playerX, minX), maxX);
+
+        transform.position = tempPos;
+    }
+}
