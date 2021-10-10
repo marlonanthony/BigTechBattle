1b0472d69f89ff5d4897f9a0075af980984aafe9
Mark Wiemer <markwiemer@outlook.com>
2021-10-09 17:31:50 -0700

    Add player 1 movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 10f;
+
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private float movementX;
+    private Rigidbody2D myBody;
+    private Animator anim;
+    private SpriteRenderer sr;
+    private const string WALK_ANIMATION = "Walk";
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        // -1, 0, or 1
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void AnimatePlayer()
+    {
+        if (movementX > 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+        }
+        else if (movementX < 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+}
